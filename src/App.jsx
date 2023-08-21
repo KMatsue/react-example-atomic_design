04d13c477169dd8b309d8b10ec761d292bf0cfa0
aho <matsue4692@gmail.com>
2023-08-21 18:46:23 +0900

    Add: molucules

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@
 import "./App.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molucules/SearchInput";
 
 function App() {
   return (
     <>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <SearchInput></SearchInput>
     </>
   );
 }
