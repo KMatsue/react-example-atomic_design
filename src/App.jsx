3fc2430b44584235f1528b07d3a9f80a6eb979f8
aho <matsue4692@gmail.com>
2023-08-21 09:11:54 +0900

    Add: BaseButton.jsx atomsのbuttonを作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 
 function App() {
   return (
     <>
       <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
     </>
   );
 }
